4f45fbe29eba7f8f5c127d825865f6b53a79cbf4
Syed Faraz <syedfarazuddin73@gmail.com>
2024-02-19 19:41:40 +0530

    dependencies added!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function App() {
-  return <div className="bg-cyan-400 h-[100vh]">hello</div>;
+  return (
+    <div className="bg-indigo-100 h-[100vh]">
+      <div className="h-20 bg-indigo-300 flex items-center justify-center">
+        <h1 className="font-extrabold text-lime-900 font-serif text-xl">
+          Shopping cart using Redux
+        </h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
