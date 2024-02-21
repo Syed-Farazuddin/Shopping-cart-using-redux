2ccd3c7920576f04e62e4372d5a2e14778ce72d2
Syed Faraz <syedfarazuddin73@gmail.com>
2024-02-21 09:26:29 +0530

    ........

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,27 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import { FaShoppingBasket, FaHome } from "react-icons/fa";
 function Nav() {
   return (
-    <div className="bg-indigo-100 h-[100vh]">
-      <div className="h-20 bg-indigo-300 flex items-center justify-center">
-        <h1 className="font-extrabold text-lime-900 font-serif text-xl">
-          Shopping cart using Redux
+    <div className="py-4 cursor-pointer  px-8 bg-indigo-300 flex items-center justify-between max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-lime-900 font-serif text-2xl hover:text-indigo-950">
+          <Link to={"/"}>Shopping cart using Redux</Link>
         </h1>
+      </div>
+      <div className="flex items-center gap-2 justify-around">
+        <ul className="flex justify-around items-center gap-5 text-2xl  text-lime-900 font-extrabold ">
+          <Link to={"/"}>
+            <li className="hover:text-indigo-950">
+              <FaHome />
+            </li>
+          </Link>
+          <Link to={"/cart"}>
+            <li className="hover:text-indigo-950">
+              <FaShoppingBasket />
+            </li>
+          </Link>
+        </ul>
       </div>
     </div>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Homepage() {
-  return <div>Homepage</div>;
+  return <div className="bg-indigo-100 h-[100vh] px-12 py-10.">Homepage</div>;
 }
 
 export default Homepage;
